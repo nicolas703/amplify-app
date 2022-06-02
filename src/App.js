8240cf6db285ca1767d583470f85e8a0b3524c32
nicolas803 <ext-nicolas.davila@jetsmart.com>
2022-06-02 01:16:17 -0400

    cambio de auth

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import React from "react";
-import "./App.css";
-import "@aws-amplify/ui-react/styles.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import ValidarSesion from "./validate";
-import Home from "./home";
+import { Authenticator } from '@aws-amplify/ui-react';
+
+import { Login } from './components/Login';
+import { Home } from './components/Home';
+import { Layout } from './components/Layout';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import './App.css';
+import { Protected } from './components/Uno';
+import { RequireAuth } from './components/RequireAuth';
+import { ProtectedSecond } from './components/Dos';
+
+function MyRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="/protected"
+            element={
+              <RequireAuth>
+                <Protected />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/protected2"
+            element={
+              <RequireAuth>
+                <ProtectedSecond />
+              </RequireAuth>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/inicsec">Iniciar Sesion</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/inicsec" element={<ValidarSesion />} />
-        </Routes>
-      </div>
-    </Router>
+    <Authenticator.Provider>
+      <MyRoutes />
+    </Authenticator.Provider>
   );
 }
 
